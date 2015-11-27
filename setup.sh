4e58986d1a1368356d01554fad17667d4bab68fc
Leandros Linux <dev@arvid-g.de>
2015-11-27 11:19:08 +0100

    add setup.sh, for easy setup

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z ${HOME+x} ]; then
+    echo "`HOME` environment variable is not set."
+    exit 1
+fi
+
+# Install a set of programs:
+#   - less
+#   - astyle
+#   - zsh
+#   - js-beautify
+#   - tig
+#   - git
+#   - tmux
+#   - vim
+
+# Copy all files over:
+cp .astylerc $HOME/
+cp .gitconfig $HOME/
+cp .jsbeautifyrc $HOME/
+cp .jshintrc $HOME/
+cp .lesskey $HOME/
+cp .tigrc $HOME/
+cp .tmux.conf $HOME/
+cp -r .vim $HOME/
+cp .vimrc $HOME/
+cp .ycm_extra_conf.py $HOME/
+cp .zshrc $HOME/
+
