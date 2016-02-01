1e54eced90c59bd0020840cc6766ba5e5725438b
Arvid Gerstmann <dev@arvid-g.de>
2016-02-01 08:48:00 +0100

    fix tmux powerline with new passwords

--- .tmux/custom/powerline/powerline-right.sh
+++ .tmux/custom/powerline/powerline-right.sh
@@ -14,7 +14,7 @@ LOAD_MEM_USE=`tmux-mem-cpu-load | grep -Eo "([0-9])+" | head -n 1`
 LOAD_MEM_ALL=`tmux-mem-cpu-load | grep -Eo "([0-9])+" | head -n 2 | tail -n 1`
 LOAD_MEM=$(printf "%.2f%%" `echo "(${LOAD_MEM_USE}/${LOAD_MEM_ALL}) * 100" | bc -l`)
 
-source ~/.passwords
+source ~/.passwords.sh
 COUNT_PRIVATE=$(curl -u $MAIL_USERNAME_PRIVATE:$MAIL_PASSWORD_PRIVATE --silent "https://mail.google.com/mail/feed/atom" | grep -E -m 1 -o '<fullcount>(.*)</fullcount>' | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g')
 COUNT_APPICO=$(curl -u $MAIL_USERNAME_APPICO:$MAIL_PASSWORD_APPICO --silent "https://mail.google.com/mail/feed/atom" | grep -E -m 1 -o '<fullcount>(.*)</fullcount>' | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g')
 COUNT_VOONY=$(curl -u $MAIL_USERNAME_VOONY:$MAIL_PASSWORD_VOONY --silent "https://mail.google.com/mail/feed/atom" | grep -E -m 1 -o '<fullcount>(.*)</fullcount>' | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g')
