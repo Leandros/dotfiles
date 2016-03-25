f42860940a7cb158a184c5e5d16edad6056d3df7
Arvid Gerstmann <dev@arvid-g.de>
2016-03-25 20:02:29 +0100

    improve setup.sh copying over files

--- setup.sh
+++ setup.sh
@@ -106,6 +106,34 @@ if [[ "$SKIPCOPY" -eq 0 ]]; then
     sed -e 's,##NEWUSER##,'"$NEWUSER"',g' .zshrc > .zshrc.tmp && mv .zshrc.tmp .zshrc
     sed -e 's,##NEWEDITOR##,'"$NEWEDITOR"',g' .zshrc > .zshrc.tmp && mv .zshrc.tmp .zshrc
 
+    rm -rf $NEWHOME/.dotbackup
+    mkdir -p $NEWHOME/.dotbackup
+    mv $NEWHOME/.astylerc $HEWHOME/.dotbackup/
+    mv $NEWHOME/.config $NEWHOME/.dotbackup/
+    mv $NEWHOME/.gitconfig $NEWHOME/.dotbackup/
+    mv $NEWHOME/.jsbeautifyrc $NEWHOME/.dotbackup/
+    mv $NEWHOME/.jshintrc $NEWHOME/.dotbackup/
+    mv $NEWHOME/.lessfilter $NEWHOME/.dotbackup/
+    mv $NEWHOME/.lesskey $NEWHOME/.dotbackup/
+    mv $NEWHOME/.lldbinit $NEWHOME/.dotbackup/
+    mv $NEWHOME/.mutt $NEWHOME/.dotbackup/
+    mv $NEWHOME/.oh-my-zsh $NEWHOME/.dotbackup/
+    mv $NEWHOME/.passwords.sh $NEWHOME/.dotbackup/
+    mv $NEWHOME/.tigrc $NEWHOME/.dotbackup/
+    mv $NEWHOME/.tmux $NEWHOME/.dotbackup/
+    mv $NEWHOME/.tmux.conf $NEWHOME/.dotbackup/
+    mv $NEWHOME/.vim $NEWHOME/.dotbackup/
+    mv $NEWHOME/.vimrc $NEWHOME/.dotbackup/
+    mv $NEWHOME/.xbindkeysrc $NEWHOME/.dotbackup/
+    mv $NEWHOME/.Xdefaults $NEWHOME/.dotbackup/
+    mv $NEWHOME/.xinitrc $NEWHOME/.dotbackup/
+    mv $NEWHOME/.Xmodmap $NEWHOME/.dotbackup/
+    mv $NEWHOME/.xsession $NEWHOME/.dotbackup/
+    mv $NEWHOME/.ycm_extra_conf.py $NEWHOME/.dotbackup/
+    mv $NEWHOME/.zsh $NEWHOME/.dotbackup/
+    mv $NEWHOME/.zshrc $NEWHOME/.dotbackup/
+
+
     # Copy all files over:
     cp .astylerc $NEWHOME/
     cp -r .config $NEWHOME/
@@ -123,6 +151,11 @@ if [[ "$SKIPCOPY" -eq 0 ]]; then
     cp .tmux.conf $NEWHOME/
     cp -r .vim $NEWHOME/
     cp .vimrc $NEWHOME/
+    cp .xbindkeysrc $NEWHOME/
+    cp .Xdefaults $NEWHOME/
+    cp .xinitrc $NEWHOME/
+    cp .Xmodmap $NEWHOME/
+    cp .xsession $NEWHOME/
     cp .ycm_extra_conf.py $NEWHOME/
     cp -r .zsh $NEWHOME/
     cp .zshrc $NEWHOME/
