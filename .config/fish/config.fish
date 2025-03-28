933ab4ded150747d7403f462fb198192f97ea75f
Arvid Gerstmann <github@arvid.io>
2025-03-28 16:32:01 +0000

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -201,6 +201,8 @@ function fish_user_key_bindings
     bind -M visual G end-of-buffer
 
     # Insert mode bindings.
+    bind -M insert ctrl-s forward-char
+    bind -M insert ctrl-b backward-char
 
     # Copy&Paste
     function x-copy
@@ -334,6 +336,40 @@ function lg
     if [ -f "$LAZYGIT_NEW_DIR_FILE" ]
         cd "$(cat $LAZYGIT_NEW_DIR_FILE)"
         rm -f $LAZYGIT_NEW_DIR_FILE >/dev/null
+    end
+end
+
+# nnn
+function n --wraps nnn --description 'support nnn quit and change directory'
+    # Block nesting of nnn in subshells
+    if test -n "$NNNLVL" -a "$NNNLVL" -ge 1
+        echo "nnn is already running"
+        return
+    end
+
+    # The behaviour is set to cd on quit (nnn checks if NNN_TMPFILE is set)
+    # If NNN_TMPFILE is set to a custom path, it must be exported for nnn to
+    # see. To cd on quit only on ^G, remove the "-x" from both lines below,
+    # without changing the paths.
+    if test -n "$XDG_CONFIG_HOME"
+        set -x NNN_TMPFILE "$XDG_CONFIG_HOME/nnn/.lastd"
+    else
+        set -x NNN_TMPFILE "$HOME/.config/nnn/.lastd"
+    end
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+    # stty lnext undef
+
+    # The command function allows one to alias this function to `nnn` without
+    # making an infinitely recursive alias
+    command nnn $argv
+
+    if test -e $NNN_TMPFILE
+        source $NNN_TMPFILE
+        rm -- $NNN_TMPFILE
     end
 end
 
