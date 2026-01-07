38f44e201f1c237e8f2bcbe72ec1ddfe2311dd9e
Arvid Gerstmann <github@arvid.io>
2026-01-07 09:24:55 +0100

    add text-case.nvim

--- .vim.init.lua
+++ .vim.init.lua
@@ -3753,6 +3753,34 @@ local spec = {
   { "tmccombs/ansify.nvim" },
 
   {
+    "johmsalas/text-case.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("textcase").setup({
+        default_keymappings_enabled = true,
+        prefix = "ge",
+      })
+      require("telescope").load_extension("textcase")
+    end,
+    keys = {
+      "ge", -- Default invocation prefix
+      { "ge.", "<cmd>TextCaseOpenTelescope<CR>", mode = { "n", "x", "v" }, desc = "Telescope" },
+    },
+    cmd = {
+      -- NOTE: The Subs command name can be customized via the option "substitude_command_name"
+      "Subs",
+      "TextCaseOpenTelescope",
+      "TextCaseOpenTelescopeQuickChange",
+      "TextCaseOpenTelescopeLSPChange",
+      "TextCaseStartReplacingCommand",
+    },
+    -- If you want to use the interactive feature of the `Subs` command right away, text-case.nvim
+    -- has to be loaded on startup. Otherwise, the interactive feature of the `Subs` will only be
+    -- available after the first executing of it or after a keymap of text-case.nvim has been used.
+    lazy = false,
+  },
+
+  {
     "folke/lazydev.nvim",
     ft = "lua",
     opts = {
