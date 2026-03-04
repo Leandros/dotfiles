486dcbe6698448345191183ae8bd8cd2acda8d76
Arvid Gerstmann <github@arvid.io>
2026-03-04 15:09:07 +0000

    remove autopairs plugins

--- .vim.init.lua
+++ .vim.init.lua
@@ -1139,12 +1139,6 @@ local spec = {
             call AutoPairsToggle()
           endif
 
-lua << EOF
-            if package.loaded["blink.pairs"] ~= nil then
-              require("blink.pairs.mappings").disable()
-            end
-EOF
-
           " Add mappings here if desired:
           " Example: nmap <buffer> b <Left>
         endfunction
@@ -1156,12 +1150,6 @@ EOF
           if exists("AutoPairsToggle")
             call AutoPairsToggle()
           endif
-
-lua << EOF
-            if package.loaded["blink.pairs"] ~= nil then
-              require("blink.pairs.mappings").enable()
-            end
-EOF
 
           " And unmap them again here:
           " Example: nunmap <buffer> b
@@ -1330,80 +1318,6 @@ EOF
     end,
   },
 
-  -- ━━ Autopairs ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-  {
-    'saghen/blink.pairs',
-    version = '*', -- (recommended) only required with prebuilt binaries
-    dependencies = 'saghen/blink.download', -- download prebuilt binaries from github releases
-
-    --- @module 'blink.pairs'
-    --- @type blink.pairs.Config
-    opts = {
-      mappings = {
-        -- you can call require("blink.pairs.mappings").enable()
-        -- and require("blink.pairs.mappings").disable()
-        -- to enable/disable mappings at runtime
-        enabled = true,
-        cmdline = true,
-        -- or disable with `vim.g.pairs = false` (global) and `vim.b.pairs = false` (per-buffer)
-        -- and/or with `vim.g.blink_pairs = false` and `vim.b.blink_pairs = false`
-        disabled_filetypes = {
-          "TelescopePrompt",
-          "TelescopeResults",
-          "Trouble",
-          "help",
-          "mason",
-          "floaterm",
-          "Terminal",
-          "gitcommit",
-          "vim-plug",
-          "lspinfo",
-          "vim",
-          "packer",
-          "checkhealth",
-          "man",
-          "guihua",
-          "guihua_rust",
-          "clap_input",
-        },
-        -- see the defaults:
-        -- https://github.com/Saghen/blink.pairs/blob/main/lua/blink/pairs/config/mappings.lua#L14
-        pairs = {
-          ['`'] = {
-            {
-              '```',
-              when = function(ctx) return ctx:text_before_cursor(2) == '``' end,
-              languages = { 'markdown', 'markdown_inline', 'typst', 'vimwiki', 'rmarkdown', 'rmd', 'quarto', 'rust' },
-            },
-          },
-        },
-      },
-      highlights = {
-        enabled = true,
-        -- requires require('vim._extui').enable({}), otherwise has no effect
-        cmdline = true,
-        groups = {
-          'BlinkPairsOrange',
-          'BlinkPairsPurple',
-          'BlinkPairsBlue',
-        },
-        unmatched_group = 'BlinkPairsUnmatched',
-
-        -- highlights matching pairs under the cursor
-        matchparen = {
-          enabled = true,
-          -- known issue where typing won't update matchparen highlight, disabled by default
-          cmdline = false,
-          -- also include pairs not on top of the cursor, but surrounding the cursor
-          include_surrounding = false,
-          group = 'BlinkPairsMatchParen',
-          priority = 250,
-        },
-      },
-      debug = false,
-    },
-  },
-
   -- ━━ autotag (autoclose html) ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
   {
     "windwp/nvim-ts-autotag",
@@ -1474,7 +1388,6 @@ EOF
       "hrsh7th/cmp-path", -- Completion for paths
       "hrsh7th/cmp-buffer", -- Completion from buffer
       "hrsh7th/cmp-cmdline", -- Completion for :... commands
-      "windwp/nvim-autopairs",
     },
     config = function()
       local cmp = require("cmp")
@@ -1612,10 +1525,6 @@ EOF
           { name = "cmdline" },
         }),
       })
-
-      -- Doesn't work, for some reason
-      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-      cmp.event:on("confirm_done", function() cmp_autopairs.on_confirm_done() end)
     end,
   }, -- end cmp
 
