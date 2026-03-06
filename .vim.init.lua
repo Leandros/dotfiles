e3a758ecbf19d5da6804b89cb92e138e16c3562b
Arvid Gerstmann <github@arvid.io>
2026-03-06 12:27:09 +0000

    enable proper rainbow delimiters

--- .vim.init.lua
+++ .vim.init.lua
@@ -3052,12 +3052,56 @@ local spec = {
 
   {
     "lukas-reineke/indent-blankline.nvim",
+    dependencies = { "HiPhish/rainbow-delimiters.nvim" },
     config = function()
+      local highlight = {
+        "RainbowRed",
+        "RainbowYellow",
+        "RainbowBlue",
+        "RainbowOrange",
+        "RainbowGreen",
+        "RainbowViolet",
+        "RainbowCyan",
+      }
       local hooks = require("ibl.hooks")
+      -- create the highlight groups in the highlight setup hook, so they are reset
+      -- every time the colorscheme changes
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+          vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+          vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+          vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+          vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+          vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+          vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+          vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+      end)
+
+      ---@type rainbow_delimiters.config
+        vim.g.rainbow_delimiters = {
+          highlight = highlight,
+          query = {
+            -- Use parentheses by default
+            [''] = 'rainbow-delimiters',
+            -- Use blocks for Lua
+            lua = 'rainbow-blocks',
+          },
+        }
+
       hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+      hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 
       function _G.ibl_setup()
+        ---@module "ibl"
+        ---@type ibl.config
         require("ibl").setup({
+          -- clashes with rainbow-delimiters
+          -- see: <https://github.com/lukas-reineke/indent-blankline.nvim/issues/555>
+          -- and: <https://github.com/HiPhish/rainbow-delimiters.nvim/issues/2#issuecomment-2461235126>
+          -- use_treesitter = false,
+
+          scope = {
+            highlight = highlight,
+          },
           indent = {
             char = "",
             -- char = "│",
